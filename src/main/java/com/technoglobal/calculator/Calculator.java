bdd5fa56de79b5cb3e783a881466ba2c95a6aa5a
fernando <ferdelangel92@gmail.com>
2023-03-17 17:35:14 -0400

    basic calculator

--- src/main/java/com/technoglobal/calculator/Calculator.java
+++ src/main/java/com/technoglobal/calculator/Calculator.java
@@ -1,16 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+/**
+   Document: Calculator.
+   Created on: Mar. 17, 2023.
+   Author: Engineer. Howard Fernando Del Angel Leija.
+**/
 
 package com.technoglobal.calculator;
 
-/**
- *
- * @author fernando
- */
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1;
+	int num2;
+	
+	String operation;
+		
+	Scanner input = new Scanner(System.in);
+	    System.out.println("please enter the first number: ");
+	    num1 = input.nextInt();
+	    
+	    System.out.println("please enter the second number: ");
+	    num2 = input.nextInt();
+	    
+	    Scanner op =  new Scanner(System.in);
+	    
+	    System.out.println("please enter the operaton");
+	    operation = op.next();
+	    
+	    if(operation == "+");
+	    {
+		    System.out.println("your answer is: " + (num1 + num2));
+	    }
+	    if(operation == "-");
+	    {
+		    System.out.println("your answer is: " + (num1 + num2));
+	    }
+	    if(operation == "*");
+	    {
+		    System.out.println("your answer is: " + (num1 + num2));
+	    }
+	    if(operation == "/");
+	    {
+		    System.out.println("your answer is: " + (num1 + num2));
+	    }
     }
 }
