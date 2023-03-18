68a6010644283774c3d3d0502a304f68aaf76110
fernando <ferdelangel92@gmail.com>
2023-03-18 02:36:08 -0400

    basic calculator

--- src/main/java/com/technoglobal/calculator/Calculator.java
+++ src/main/java/com/technoglobal/calculator/Calculator.java
@@ -6,51 +6,129 @@
 
 package com.technoglobal.calculator;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         
-	    System.out.println("1. Addition");
-	    System.out.println("2. Substraction");
-	    System.out.println("3. Multiplication");
-	    System.out.println("4. Divition");
-	    System.out.println("0. Exit");
-	    System.out.println("\n");
+	System.out.println("1. Addition");
+	System.out.println("2. Substraction");
+	System.out.println("3. Multiplication");
+	System.out.println("4. Division");
+	System.out.println("0. Exit");
+	System.out.println("\n");
 	    
-	int num1;
-	int num2;
+	System.out.println("Enter your choice");
 	
-	String operation;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String str = br.readLine();
+	
+	int usrChoice = Integer.parseInt(str);
 		
-	Scanner input = new Scanner(System.in);
-	    System.out.println("please enter the first number: ");
-	    num1 = input.nextInt();
-	    
-	    System.out.println("please enter the second number: ");
-	    num2 = input.nextInt();
-	    
-	    Scanner op =  new Scanner(System.in);
-	    
-	    System.out.println("please enter the operaton");
-	    operation = op.next();
-	    
-	    if(operation == "+");
-	    {
-		    System.out.println("your answer is: " + (num1 + num2));
+	    switch (usrChoice) {
+		    case 1:
+			    doAddition();
+			    break;
+		    case 2:
+			    doSubstraction();
+			    break;
+		    case 3:
+			    doMultiplication();
+			    break;
+		    case 4:
+			    doDivision();
+			    break;
+		    case 0:
+			    doAddition();
+			    System.out.println("Goodbye :D");
+		    default:
+			    System.out.println("Invalid Value");
 	    }
-	    if(operation == "-");
-	    {
-		    System.out.println("your answer is: " + (num1 - num2));
-	    }
-	    if(operation == "*");
-	    {
-		    System.out.println("your answer is: " + (num1 * num2));
-	    }
-	    if(operation == "/");
-	    {
-		    System.out.println("your answer is: " + (num1 / num2));
-	    }
+	    
+	    System.out.println(":::::::::::::::::::::End:::::::::::::::::::");
     }
+    
+    public static void doAddition() throws Exception{
+	    System.out.println("******* Enter in Addition Process ********");
+	    
+	    System.out.println("Enter number: ");
+	    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	    strNo1 = br.readLine();
+	    
+	    System.out.println("Enter number: ");
+	    BufferedReader br1 = BufferedReader(new InputStreamReader(System.in));
+	    strN2 = br1.readLine();
+	    
+	    int no1 = Integer.parseInt(strNo1);
+	    int no2 = Integer.parseInt(strNo2);
+	    
+	    int result = no1 + no2;
+	    
+	    System.out.println("Addition of " + no1 + " and " + no2 + "is: " + result);
+    }
+    
+	    public static void doSubstraction() throws Exception {
+		    System.out.println("******* Enter in Substraction Process ********");
+		    
+		    String strNo1, strNo2;
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br = BufferedReader(new InputStreamReader(System.in));
+		    strNo1 = br.readLine();
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br1 = BufferedReader(new InputStreamReader(System.in));
+		    strNo2 = br1.readLine();
+		    
+		    int no1 = Integer.parseInt(strNo1);
+		    int no2 = Integer.parseInt(strNo2);
+		    
+		    int result = no1 - no2;
+		    
+		    System.out.println("Substraction of " + no1 + " and " + no2 + "is: " + result);
+	    }
+	    
+	    public static void doMultiplication() throws Exception {
+		    System.out.println("******* Enter in Multiplication Process ********");
+		    
+		    String strNo1, strNo2;
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    strNo1 = br.readLine();
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		    strNo2 = br1.readLine();
+		    
+		    int no1 = Integer.parseInt(strNo1);
+		    int no2 = Integer.parseInt(strNo2);
+		    
+		    int result = no1 * no2;
+		    
+		    System.out.println("Multiplication of " + no1 + " and " + no2 + "is: ");
+	    }
+	    
+	    public static void doDivision() throws Exception{
+		    System.out.println("******* Enter in Dividion Process ********");
+		    
+		    String strNo1, strNo2;
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    strNo1 = br.readLine();
+		    
+		    System.out.println("Enter number: ");
+		    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		    str2 = br1.readLine();
+		    
+		    int no1 = Integer.parseInt(strNo1);
+		    int no2 = Integer.parseInt(strNo2);
+		    
+		    float result = no1 / no2; 
+		    
+		    System.out.println("division of " + no1 + " and " + no2 + "is: " + result);
+	    }
 }
